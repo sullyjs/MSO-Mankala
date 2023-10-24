8022eaee6c4368ddf3e940dbccd739f2359253dd
LeilaKaraca <91898685+LeilaKaraca@users.noreply.github.com>
2023-10-24 20:24:50 +0200

    UserInterface en ConsoleUI class

---
+++ MSO2/ConsoleUI.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace MSO2
+{
+	public class ConsoleUI : UserInterface
+	{
+		public ConsoleUI()
+		{
+		}
+
+        public override void TekenBord()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void TekenKuiltje()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ MSO2/UserInterface.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace MSO2
+{
+	public abstract class UserInterface
+	{
+		public UserInterface()
+		{
+		}
+
+		public abstract void TekenBord();
+
+		public abstract void TekenKuiltje();
+	}
+}
+
