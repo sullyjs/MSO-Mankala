db17ca6ff07b1aad044d3cbc8f0ce16831d7cc99
LeilaKaraca <91898685+LeilaKaraca@users.noreply.github.com>
2023-10-24 20:16:00 +0200

    SpelUitvoeren en SpelOpzetten toegevoegd

---
+++ MSO2/SpelOpzetten.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace MSO2
+{
+	public class SpelOpzetten
+	{
+        SpelUitvoeren spelLogica;
+        UserInputHandler inputHandler = UserInputHandler.GetInstance();
+
+        public SpelOpzetten()
+		{
+            spelLogica = new SpelUitvoeren();
+		}
+
+		void VariantSpelKiezen()
+		{
+            Console.WriteLine("Welcome to a new game of mankala: choose A to play classic mankala, and B for a variant.");
+
+            if (inputHandler.ChooseGame() == 1)
+            {
+                spelLogica.spel = new MankalaSpel();
+                Console.WriteLine("\nYou've created a new game of classic Mankala!");
+                spelLogica.spel.DrawMancalaBoard();
+                spelLogica.gameActive = true;
+            }
+            else if (inputHandler.ChooseGame() == 2)
+            {
+                spelLogica.spel = new VariantSpel();
+                Console.WriteLine("\nThis is a variant game mode that isn't implemented yet.");
+                spelLogica.gameActive = true;
+            }
+            else
+            {
+                Console.WriteLine("invalid choice.");
+            }
+        }
+
+        public static void Main()
+        {
+            SpelOpzetten spel = new SpelOpzetten();
+
+            spel.VariantSpelKiezen();
+
+            spel.spelLogica.RunGame();
+        }
+    }
+}
+
---
+++ MSO2/SpelUitvoeren.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace MSO2
+{
+    class SpelUitvoeren
+    {
+
+        internal Spel spel;
+        UserInputHandler inputHandler = UserInputHandler.GetInstance();
+        internal bool gameActive;
+
+        public SpelUitvoeren()
+        {
+            gameActive = false;
+        }
+
+        public void RunGame()
+        {
+            while (gameActive)
+            {
+
+                inputHandler.IfKeyPressed();
+                int gekozenKuiltje = inputHandler.GekozenKuiltje;
+
+                if (gekozenKuiltje != -1)
+                {
+                    spel.gekozenKuiltje = gekozenKuiltje;
+                    spel.Speel();
+
+                    if (spel.IsGameOver())
+                    {
+                        spel.DeterMineWinner();
+                        gameActive = false;
+                        Console.WriteLine("Game Over!");
+                    }
+                } 
+
+             }
+
+        }
+    }
+}
